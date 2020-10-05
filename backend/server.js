904f09f912e5ed8bf6032bd9fa4f5013cfe1b1bd
John Doe <firewlker21@yahoo.com>
2020-10-04 20:43:21 -0500

    added routes/ fetching products from DB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import products from './data/products.js'
+
 import colors from 'colors'
 import dotenv from 'dotenv'
 import connectDB from '../backend/config/db.js'
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -14,14 +15,8 @@ app.get('/', (req, res) => {
   res.send('API running with nodemon')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id)
-  res.json(product)
-})
 const PORT = process.env.PORT
 app.listen(
   PORT || 5000,
