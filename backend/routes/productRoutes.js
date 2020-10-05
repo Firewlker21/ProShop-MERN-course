96707dd6b74a6c4e220b2d9e9729488001f54571
John Doe <firewlker21@yahoo.com>
2020-10-04 21:15:51 -0500

    added custom error handling with error middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,32 +1,36 @@
 import express from 'express'
 import asyncHandler from 'express-async-handler'
 import Product from '../models/productModel.js'
-const router  = express.Router()
+const router = express.Router()
 
+// @desc Fetch all products
+// @route GET /api/products
+// @access Public
 
-  // @desc Fetch all products
-  // @route GET /api/products 
-  // @access Public
-
-  router.get('/', asyncHandler(async (req, res) => {
-
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
     const products = await Product.find({})
     res.json(products)
-  }))
-  
-   // @desc Fetch product by Id
-  // @route GET /api/products/:id 
-  // @access Public
+  })
+)
 
-  router.get('/:id', asyncHandler(async (req, res) => {
-    const product =  await Product.findById(req.params.id)
+// @desc Fetch product by Id
+// @route GET /api/products/:id
+// @access Public
 
-    if(product){
-        res.json(product)
-    }else{
-        res.status(404).json({message:'Product not found'})
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+      res.json(product)
+    } else {
+      res.status(404)
+      throw new Error('Product not Found')
     }
-    
-  }))
+  })
+)
 
-  export default router
+export default router
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import colors from 'colors'
 import dotenv from 'dotenv'
 import connectDB from '../backend/config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -16,6 +17,10 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(
