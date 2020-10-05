d62169c4206d88df47644a832e570d862fa8926a
John Doe <firewlker21@yahoo.com>
2020-10-04 22:35:10 -0500

    added redux with redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import store from './store'
+import { Provider } from 'react-redux'
 import './bootstrap.min.css'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 )
 
