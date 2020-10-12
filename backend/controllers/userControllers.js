8425354fd81efaedb280ba01640192bb7623da6e
Brandon Richter <firewlker21@yahoo.com>
2020-10-12 08:42:19 -0500

    added jwt

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
 
 // @desc Auth users and get token
 // @route POST /api/users/login
@@ -17,7 +18,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     })
   } else {
     res.status(401)
