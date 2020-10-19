bc8f157e89665643cdadbe6da91401cac4a5c742
Brandon Richter <firewlker21@yahoo.com>
2020-10-19 07:05:20 -0500

    added user regisration page

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -36,8 +36,8 @@ userSchema.pre('save', async function (next) {
     next()
   }
 
-  const salt = bcrypt.genSalt(10)
-  this.password = bcrypt.hash(this.password, salt)
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
 })
 
 const User = mongoose.model('User', userSchema)
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,12 +7,14 @@ import {
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { userLoginReducer } from './reducers/userReducers'
+import { userRegisterReducer } from './reducers/userReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDeatilsReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
