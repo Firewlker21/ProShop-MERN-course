167802000c23e56b7bbcb0045ac72c5a840da26f
Brandon Richter <firewlker21@yahoo.com>
2020-11-12 21:34:06 -0600

    added update order to paid function

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -54,4 +54,31 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 })
 
-export { addOrderItems, getOrderById }
+// @desc update order to paid
+// @route PUT/api/orders/:id/pay
+// @access Private
+
+const updateOrdertoPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+  
+
+  if (order) {
+    order.isPaid = true,
+    order.PaidAt = Date.now(),
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.staus,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+
+    }
+    const updatedOrder = await order.save()
+
+    res.json(updatedOrder)
+  } else {
+    res.status(404)
+    throw new Error('No order found')
+  }
+})
+
+export { addOrderItems, getOrderById, updateOrdertoPaid }
